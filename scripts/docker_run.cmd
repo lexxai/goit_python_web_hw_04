541e40fe53d94e6e34b127be8fa0a66d3f1fcded
lexxai <lexxai@users.noreply.github.com>
2023-09-05 16:35:06 +0300

    show  volume

--- scripts/docker_run.cmd
+++ scripts/docker_run.cmd
@@ -1,8 +1,9 @@
 @echo off
 PUSHD ..\tests
 
-   
-docker run -it -d --rm -p 3000:3000  -v demo-web_hw_04_volume:/app/storage  --name web_hw_04  lexxai/web_hw_04     
+docker run -it -d --rm -p 3000:3000  -v web_hw_04_volume:/app/storage  --name web_hw_04  lexxai/web_hw_04   
+
+docker volume ls
                     
 
 POPD
