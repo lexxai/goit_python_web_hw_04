c11f1809ee10af55264c6eb48e4c04a246e568a8
lexxai <lexxai@users.noreply.github.com>
2023-09-05 14:40:34 +0300

    POST

--- webapp/app.py
+++ webapp/app.py
@@ -2,6 +2,7 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 from pathlib import Path
 import urllib.parse
 import mimetypes
+import json
 
 
 class WWWHandler(BaseHTTPRequestHandler):
@@ -21,19 +22,45 @@ class WWWHandler(BaseHTTPRequestHandler):
             self.send_header("Content-Type", "plain/text")
         self.end_headers()
         try:
-            with open(self.BASE_ROOT_DIR / filename, "rb") as fp:
+            with open(filename, "rb") as fp:
                 self.wfile.write(fp.read())
         except FileNotFoundError as e:
             print(e)
+
+    def do_POST(self):
+        route_path = urllib.parse.urlparse(self.path)
+        match route_path.path:
+            case "/message":
+                cont_len = int(self.headers["Content-Length"])
+                data = self.rfile.read(cont_len).decode()
+                data_parse = { 
+                    key: urllib.parse.unquote_plus(val) for key, val in [ el.split("=") for el in data.split("&")]
+                }
+                try:
+                    # print(data_parse)
+                    json_data = json.dumps(data_parse, ensure_ascii=False)
+                    self.send_response(200)
+                    self.send_header("Content-Type", "application/json; charset=utf-8")
+                    self.end_headers()
+                    result = json_data
+                    self.wfile.write(result.encode())
+                    print(f"{result}")
+                except Exception as e:
+                     print(e)
+            case _:
+                self.send_response(301)
+                self.send_header("Location", "/error.html")
+                self.end_headers()
+
 
     def do_GET(self):
         route_path = urllib.parse.urlparse(self.path)
         match route_path.path:
             case "/":
-                filename = "index.html"
+                filename = self.BASE_ROOT_DIR / "index.html"
                 self.get_file(filename)
             case _:
-                filename = self.BASE_ROOT_DIR / route_path.pat[1:]
+                filename = self.BASE_ROOT_DIR / route_path.path[1:]
                 if filename.exists():
                     self.get_file(filename)
                 else:
@@ -43,7 +70,7 @@ class WWWHandler(BaseHTTPRequestHandler):
 
 def run(server=HTTPServer, handler=WWWHandler):
     address = ("", 3000)
-    handler.set_root(Path("../www-data"))
+    handler.set_root(Path("www-data/"))
     http_server = server(address, handler)
     try:
         http_server.serve_forever()
