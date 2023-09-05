78236e87a731c51c9a2978710f37cdaf23d0b58e
lexxai <lexxai@users.noreply.github.com>
2023-09-05 13:04:24 +0300

    urlparse

--- webapp/app.py
+++ webapp/app.py
@@ -1,18 +1,34 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from pathlib import Path
+import urllib.parse
 
 BASE_ROOT_DIR = Path("../www-data")
 
 
 class WWWHandler(BaseHTTPRequestHandler):
 
-    def do_GET(self):
-        self.send_response(200)
+    def get_file(self, filename, state=200):
+        self.send_response(state)
         self.send_header('Content-Type', 'text/html')
         self.end_headers()
         filename = "index.html"
         with open(BASE_ROOT_DIR / filename, "rb") as fp:
-            self.wfile.write(fp.read()) 
+            self.wfile.write(fp.read())         
+
+    def do_GET(self):
+        req_path = urllib.parse.urlparse(self.path).path
+        match req_path:
+            case '/':
+                filename = 'index.html'
+                self.get_file(filename)
+            case _:
+                filename = BASE_ROOT_DIR / req_path[1:]
+                if  filename.exists():
+                    self.get_file(filename)
+                else:
+                    filename = BASE_ROOT_DIR / 'error.html'
+                    self.get_file(filename, 404)
+
 
 
 
